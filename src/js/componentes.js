0db6bc8391d528715b863720d971e94dd3e16c93
Paserno <paserno.x@gmail.com>
2021-11-20 15:08:23 -0300

    Aplicar Filtro
    Se agrega el evento filtro, el cual selecciona el texto, y le aplica el (hidde) a los elementos q no se esten filtrando

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -3,9 +3,11 @@ import { Todo } from "../classes";
 import { todoList } from "../index";
 
 // * Referencia en el HTMÑ
-const divTodoList = document.querySelector('.todo-list');
-const txtInput = document.querySelector('.new-todo');
-const btnBorrar = document.querySelector('.clear-completed');
+const divTodoList   = document.querySelector('.todo-list');
+const txtInput      = document.querySelector('.new-todo');
+const btnBorrar     = document.querySelector('.clear-completed');
+const ulFiltros     = document.querySelector('.filters');
+const anchorFiltros = document.querySelectorAll('.filtro')
 
 
 export const crearTodoHtml = (todo) => {
@@ -76,8 +78,41 @@ btnBorrar.addEventListener('click', () => {
         if (elemento.classList.contains('completed')) {
             divTodoList.removeChild(elemento);
         }
+    }
+});
+
+ulFiltros.addEventListener('click', (event) => {
+    const filtro = event.target.text; // existe la probabilidad de que mande undefined, si no haces clic en el texto
+    if (!filtro){return;} // esto lo que haria es retornar y salir de este evento
+
+    anchorFiltros.forEach(elem => elem.classList.remove('selected')); 
+    event.target.classList.add('selected');
+
+
+    for (const elemento of divTodoList.children) {
+
+        elemento.classList.remove('hidden'); 
+        const completado = elemento.classList.contains('completed');
+
+        switch(filtro){
+
+            case 'Pendientes':
+                if (completado) {
+                    elemento.classList.add('hidden');
+                    // console.log(completado);
+                    }
+            break;
+
+            case 'Completados':
+                if (!completado) {
+                    elemento.classList.add('hidden');
+                    // console.log(completado);
+                    }
+            break;            
+
+        }
 
     }
-
-
-});
+        
+    
+})
